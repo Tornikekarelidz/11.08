196c2dc967c369c8a563f1d287e611546297707f
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-08-11 17:51:24 +0400

    Add files via upload

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@
 #     print("რიცხვი",num2,"ლუწია")
 #     num2 = num2 + 2
 
-#HOMEWORK NUM-2
+#HOMEWORK NUM-2 Pirveli versia
 # age = int(input("შეიყვანე შენი ასაკი: "))
 # dad_age = int(input("შეიყვანე მამაშენის ასაკი: "))
 # if dad_age == age * 2:
@@ -16,16 +16,46 @@
 # else:
 #     print("mouse")
 
+# HOMEWORK NUM-2 Meore versia
+num = 0
+while num < 100:
+    if num == 1:
+        print(num)
+    else:
+        print("ეს რიცხვი -",num + 1,"- კენტია")
+    if num == 1:
+        print(num)
+    else:
+        print("ეს რიცხვი -",num + 2,"- ლუწია")
+        num = num + 2
+
+
 #HOMEWORK NUM-3
-age = int(input("შიეყვანე შენი ასაკი: "))
-sqesi = input("რა სქესს წარმოადგენთ: ")
+# age = int(input("შიეყვანე შენი ასაკი: "))
+# sqesi = input("რა სქესს წარმოადგენთ: ")
 
-if age >= 65:
-    print("თქვენ ხართ ხანში შესული ამიტომ")
-else:
-    print("თქვენთვის ჯერ პენსია ადრეა *პანჩური* ")
+# if age >= 65:
+#     print("თქვენ ხართ ხანში შესული ამიტომ")
+# else:
+#     print("თქვენთვის ჯერ პენსია ადრეა *პანჩური* ")
 
-if sqesi == "ქალი" or sqesi == "მამაკაცი" or sqesi == "ქალბატონი" or sqesi == "კაცი":
-    print("თქვენ გეკუთვნით პენსია")
-else:
-    print("შენ არხარ ამქვეყნიური არსება *პანჩური* ")
+# if sqesi == "ქალი" or sqesi == "მამაკაცი" or sqesi == "ქალბატონი" or sqesi == "კაცი":
+#     print("თქვენ გეკუთვნით პენსია")
+# else:
+#     print("შენ არხარ ამქვეყნიური არსება *პანჩური* ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
